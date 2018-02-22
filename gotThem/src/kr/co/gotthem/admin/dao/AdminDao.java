b3304b077b29be5a824ef8fa7fc2b34b14fecb29
Bit <Bit@Bitcamp>
2018-02-22 15:26:05 +0900

    2018.02.22

--- gotThem/src/kr/co/gotthem/admin/dao/AdminDao.java
+++
@@ -1,9 +0,0 @@
-package kr.co.gotthem.admin.dao;
-
-import java.util.List;
-import kr.co.gotthem.member.bean.MemberBean;
-import kr.co.gotthem.store.bean.StoreBean;
-
-public interface AdminDao {
-	public List<StoreBean> list();
-}
--- gotThem/src/kr/co/gotthem/admin/service/AdminService.java
+++
@@ -1,9 +0,0 @@
-package kr.co.gotthem.admin.service;
-
-import java.util.List;
-
-import kr.co.gotthem.store.bean.StoreBean;
-
-public interface AdminService {
-	public List<StoreBean> list();
-}
