1a564a6be8943faeb9d7463f4f076ab860bddc7e
Bit <Bit@Bitcamp>
2018-02-27 15:03:15 +0900

    2-18 02 27 통합전

--- gotThem/src/kr/co/gotthem/member/controller/MemberController.java
+++ gotThem/src/kr/co/gotthem/member/controller/MemberController.java
@@ -137,7 +137,10 @@ public class MemberController {
 	}
 	
 	@RequestMapping(value = "/memberModi.gt", method = RequestMethod.POST)
-	public String memberUpdate(MemberBean bean) {
+	public String memberUpdate(MemberBean bean, @RequestParam("mem_address1") String address1,
+			@RequestParam("mem_address2") String address2,@RequestParam("mem_post") String post) {
+		String mem_address = post + "/" + address1 + "/" + address2;
+		bean.setMem_address(mem_address);
 		memberService.memberModifi(bean);
 		return "member/mypage";
 	}
